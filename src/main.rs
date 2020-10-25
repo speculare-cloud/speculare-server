f2e21d8e9b5a40edd5a084d0341d0afb57a24d9c
Martichou <martichou.andre@gmail.com>
2020-10-25 13:25:16 +0100

    Move construct of vec/data outside the transaction

--- src/main.rs
+++ src/main.rs
@@ -25,25 +25,45 @@ pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[post("/endpoints")]
 async fn endpoints(db: web::Data<Pool>, item: web::Json<SData>) -> Result<HttpResponse, AppError> {
-    let conn = db.get()?;
-
     if log_enabled!(log::Level::Info) {
         info!("endpoints : {:?}", item);
     }
 
+    // Construct the data to insert into the db
+    let new_data = Data {
+        os: &item.os,
+        hostname: &item.hostname,
+        uptime: item.uptime,
+        uuid: &item.uuid,
+        cpu_freq: item.cpu_freq,
+        active_user: &item.user,
+        mac_address: &item.mac_address,
+        created_at: Utc::now().naive_local(),
+    };
+    // Retrieve sensors list from the item
+    let mut new_sensors: Vec<InsSensors> = Vec::with_capacity(item.sensors.len());
+    for s in &item.sensors {
+        new_sensors.push(InsSensors {
+            label: &s.label,
+            temp: s.temp,
+            data_uuid: &item.uuid,
+        });
+    }
+    // Retrieve disks list from the item
+    let mut new_disks: Vec<InsDisks> = Vec::with_capacity(item.disks.len());
+    for s in &item.disks {
+        new_disks.push(InsDisks {
+            disk_name: &s.name,
+            mount_point: &s.mount_point,
+            total_space: s.total_space,
+            avail_space: s.avail_space,
+            data_uuid: &item.uuid,
+        });
+    }
+
+    let conn = db.get()?;
     // We use a transaction so that if one of the below fail, the previous will be reverted
     conn.transaction::<_, AppError, _>(|| {
-        // Construct the data to insert into the db
-        let new_data = Data {
-            os: &item.os,
-            hostname: &item.hostname,
-            uptime: item.uptime,
-            uuid: &item.uuid,
-            cpu_freq: item.cpu_freq,
-            active_user: &item.user,
-            mac_address: &item.mac_address,
-            created_at: Utc::now().naive_local(),
-        };
         // Insert or update if conflict
         insert_into(data)
             .values(&new_data)
@@ -51,28 +71,8 @@ async fn endpoints(db: web::Data<Pool>, item: web::Json<SData>) -> Result<HttpRe
             .do_update()
             .set(&new_data)
             .execute(&conn)?;
-        // Retrieve sensors list from the item
-        let mut new_sensors: Vec<InsSensors> = Vec::new();
-        for s in &item.sensors {
-            new_sensors.push(InsSensors {
-                label: &s.label,
-                temp: s.temp,
-                data_uuid: &item.uuid,
-            });
-        }
         // Insert the sensors
         insert_into(sensors).values(&new_sensors).execute(&conn)?;
-        // Retrieve disks list from the item
-        let mut new_disks: Vec<InsDisks> = Vec::new();
-        for s in &item.disks {
-            new_disks.push(InsDisks {
-                disk_name: &s.name,
-                mount_point: &s.mount_point,
-                total_space: s.total_space,
-                avail_space: s.avail_space,
-                data_uuid: &item.uuid,
-            });
-        }
         // Insert the disks
         insert_into(disks).values(&new_disks).execute(&conn)?;
         Ok(())

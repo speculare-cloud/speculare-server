9a9ebb1190c17b2183241fcbe523ee3241268df2
Martichou <martichou.andre@gmail.com>
2020-12-27 22:30:28 +0100

    insert: optimize insert if only one item

--- src/models/hosts.rs
+++ src/models/hosts.rs
@@ -34,16 +34,27 @@ pub struct Host {
 }
 
 impl Host {
-    /// Insert the host data (update or create)
+    /// Insert the host data (update or create) (multiple value at once (Vec<HttpPostHost>))
     /// # Params
     /// * `conn` - The r2d2 connection needed to fetch the data from the db
-    /// * `item` - The HttpPostHost we just got from the Post request (contains all our info)
+    /// * `items` - The Vec<HttpPostHost> we just got from the Post request (contains all our info)
     pub fn insert(conn: &ConnType, items: &[HttpPostHost]) -> Result<(), AppError> {
-        // TODO - Maybe we can optimize this by specifying a capacity (if possible)
-        // Even if this method is more memory hungry, it prefer speed over RAM usage.
+        // If there is only one item, it's faster to only insert one
+        // == avoid allocation of vector
+        if items.len() == 1 {
+            return Host::insert_one(conn, &items[0]);
+        }
+        // Even if this method (using Vec) use more memory, it prefer speed over low RAM usage.
         // For the first three (v_ncpuinfo, v_nloadavg, v_nmemory) we init them with a capacity
-        // as in the best case, there will only be items.len() elements for each.
+        // because in the best case, there will only be items.len() elements for each.
         // For v_ndisks and v_niostats we cannot predict the numbers of elements.
+        //      On this last point, we can in fact. We can simply check the len of
+        //        - items[0].disks.len()
+        //        - items[0].iostats.len()
+        //      But as they are Optional, we also have to check if they are .is_some()
+        //      Doing so would be optimal to avoid allocations, and if they are .is_none()
+        //      we could skip the construction of new_disks and new_iostats, as well as their
+        //      respective insert.
         let mut v_ncpuinfo: Vec<NewCpuInfo> = Vec::with_capacity(items.len());
         let mut v_nloadavg: Vec<NewLoadAvg> = Vec::with_capacity(items.len());
         let mut v_nmemory: Vec<NewMemory> = Vec::with_capacity(items.len());
@@ -74,8 +85,7 @@ impl Host {
                 v_niostats.append(value_iostats);
             }
 
-            // Insert all the Host data
-            // for Host, if conflict, only update uptime
+            // Insert Host data, if conflict, only update uptime
             insert_into(hosts)
                 .values(&new_data)
                 .on_conflict(uuid)
@@ -89,6 +99,40 @@ impl Host {
         insert_into(memory).values(&v_nmemory).execute(conn)?;
         insert_into(disks).values(&v_ndisks).execute(conn)?;
         insert_into(iostats).values(&v_niostats).execute(conn)?;
+        // If we reached this point, everything went well so return an empty Closure
+        Ok(())
+    }
+
+    /// Insert the host data (update or create) (one value of HttpPostHost at a time)
+    /// # Params
+    /// * `conn` - The r2d2 connection needed to fetch the data from the db
+    /// * `item` - The Vec<HttpPostHost>[0] we just got from the Post request (contains all our info)
+    pub fn insert_one(conn: &ConnType, item: &HttpPostHost) -> Result<(), AppError> {
+        // Construct the new Struct from item
+        let new_data = Host::from(item);
+        let new_cpuinfo = NewCpuInfo::from(item);
+        let new_loadavg = Option::<NewLoadAvg>::from(item);
+        let new_memory = Option::<NewMemory>::from(item);
+        let new_disks = Option::<NewDisksList>::from(item);
+        let new_iostats = Option::<NewIostatsList>::from(item);
+
+        // Insert Host data, if conflict, only update uptime
+        insert_into(hosts)
+            .values(&new_data)
+            .on_conflict(uuid)
+            .do_update()
+            .set(uptime.eq(item.uptime))
+            .execute(conn)?;
+        // Insert data from the previous Struct we constructed
+        insert_into(cpu_info).values(&new_cpuinfo).execute(conn)?;
+        insert_into(load_avg).values(&new_loadavg).execute(conn)?;
+        insert_into(memory).values(&new_memory).execute(conn)?;
+        if let Some(value) = new_disks {
+            insert_into(disks).values(&value).execute(conn)?;
+        }
+        if let Some(value) = new_iostats {
+            insert_into(iostats).values(&value).execute(conn)?;
+        }
         // If we reached this point, everything went well so return an empty Closure
         Ok(())
     }

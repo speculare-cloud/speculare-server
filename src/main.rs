660b92d2f27b92896ada19e7cdf3fdc7e757c682
Martichou <martichou.andre@gmail.com>
2021-02-14 22:48:44 +0100

    Custom logging init - based on ENV variable

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate log;
 
 mod errors;
 mod handlers;
+mod logger;
 mod models;
 mod routes;
 mod server;
@@ -14,10 +15,8 @@ mod types;
 async fn main() -> std::io::Result<()> {
     // Load env variable from .env
     dotenv::dotenv().ok();
-    // Define the verbose of the logs - info for general and actix
-    std::env::set_var("RUST_LOG", "info,actix_server=info,actix_web=info");
-    // Init the log module
-    env_logger::init();
+    // Init the logger and set the debug level correctly
+    logger::configure();
     // Continue the initialization of the actix web server
     // And wait indefinietly for it <3
     server::server().await

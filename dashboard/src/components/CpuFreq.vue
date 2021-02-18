7701c915bea40d80b671af29dd624c1196631acb
Martichou <martichou.andre@gmail.com>
2021-02-18 20:58:20 +0100

    switch to uPlot as definitive chart library

--- dashboard/src/components/CpuFreq.vue
+++ dashboard/src/components/CpuFreq.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="cpufreq">
-		<LineChart :chartdata="datacollection" :options="chartOptions" :minvalue="chartMinval"/>
+		<LineChart :chartdata="datacollection" :options="chartOptions"/>
 	</div>
 </template>
 
@@ -12,60 +12,62 @@ export default {
 	props: ['uuid'],
 	connection: null,
 	components: {
-		LineChart,
+		LineChart
 	},
 
 	data () {
 		return {
 			datacollection: null,
 			chartLabels: [],
-			chartDataObj: null,
-			chartMinval: null,
+			chartDataObj: [],
 			chartOptions: {
-				legend: {
-            		display: false
-          		},
-				elements: {
-      				line: {
-        				tension: 0
-      				},
-					point:{
-                        radius: 0,
-                    }
-    			},
-				hover: {
-					mode: 'nearest',
-					intersect: true
-				},
-				tooltips: {
-					mode: 'index',
-					intersect: false,
-				},
-				scales: {
-            		xAxes: [{
-                		type: 'time',
-                		time: {
-                    		unit: 'second',
-							unitStepSize: 30
-                		},
-						gridLines: {
-    						display: false,
-  						},
-						ticks: {
-        					min : null,
+				...this.getSize(),
+				cursor: {
+					dataIdx: (self, seriesIdx, hoveredIdx) => {
+						let seriesData = self.data[seriesIdx];
+
+						if (seriesData[hoveredIdx] == null) {
+							let nonNullLft = hoveredIdx,
+								nonNullRgt = hoveredIdx,
+								i;
+
+							i = hoveredIdx;
+							while (nonNullLft == hoveredIdx && i-- > 0)
+								if (seriesData[i] != null)
+									nonNullLft = i;
+
+							i = hoveredIdx;
+							while (nonNullRgt == hoveredIdx && i++ < seriesData.length)
+								if (seriesData[i] != null)
+									nonNullRgt = i;
+
+							return nonNullRgt - hoveredIdx > hoveredIdx - nonNullLft ? nonNullLft : nonNullRgt;
 						}
-            		}],
-					yAxes: [{
-      					gridLines: {
-							display: false,
-      					},
-            			ticks: {
-                			beginAtZero: true
-            			}
-    				}]
-        		},
-				responsive: true,
-      			maintainAspectRatio: false
+
+						return hoveredIdx;
+					},
+					drag: {
+						setScale: false,
+					}
+				},
+				select: {
+					show: false,
+				},
+				series: [
+					{},
+					{
+						stroke: "red",
+						fill: "rgba(255,0,0,0.05)",
+					}
+				],
+				scales: {
+					x: {
+						time: true,
+					},
+					y: {
+						auto: true,
+					},
+				}
 			}
 		}
 	},
@@ -78,39 +80,43 @@ export default {
 			vm.connection = new WebSocket("wss://cdc.speculare.cloud:9641/ws?change_table=cpu_info&specific_filter=host_uuid.eq." + vm.uuid);
 		}
 
-		let count = 0;
-
 		this.connection.onmessage = function(event) {
 			let json = JSON.parse(event.data);
 			let newValues = json["columnvalues"];
 
 			let date_with_no_ms = newValues[3].replace(/\.\d+/, "");
-			vm.chartLabels.push(date_with_no_ms);
+			let date_obj = (new Date(date_with_no_ms) / 1000);
+			vm.chartLabels.push(date_obj);
+			vm.chartDataObj.push(newValues[1]);
 
-			// Update the min date
-			let tmp = new Date(date_with_no_ms);
-			tmp.setMinutes(tmp.getMinutes() - 5);
-			vm.chartMinval = tmp;
-
-			if (vm.chartDataObj == null) {
-				vm.chartDataObj = {
-            		label: 'CpuFreq',
-            		backgroundColor: '#f87979',
-	            	data: [newValues[1]]
-            	};
-			} else {
-				vm.chartDataObj.data.push(newValues[1]);
-			}
-
-			if (vm.chartDataObj.data.length > (60 * 5) + 15) {
-				vm.chartDataObj.data.shift();
+			if (vm.chartDataObj.length > (60 * 5) + 15) {
 				vm.chartLabels.shift();
+				vm.chartDataObj.shift();
 			}
 
-			vm.datacollection = {
-				labels: vm.chartLabels,
-				datasets: [vm.chartDataObj]
-			};
+			if (vm.datacollection == null) {
+				// If the newData[1] contains more than 4000 items, use a for loop
+				// https://medium.com/coding-at-dawn/the-fastest-way-to-find-minimum-and-maximum-values-in-an-array-in-javascript-2511115f8621
+				let max = Math.max.apply(null, vm.chartDataObj);
+				vm.chartOptions.scales.y = {
+					auto: false,
+					range: [0, max + (max / 10)]
+				}
+			}
+
+			vm.datacollection = [
+				vm.chartLabels,
+				vm.chartDataObj,
+			];
+		}
+	},
+
+	methods: {
+		getSize: function() {
+			return {
+				width: window.innerWidth,
+				height: 300,
+			}
 		}
 	},
 
--- dashboard/src/components/LineChart.vue
+++ dashboard/src/components/LineChart.vue
@@ -1,67 +1,81 @@
 <template>
-	<canvas id="lineChart" @mouseover="hover = true" @mouseleave="hover = false"></canvas>
+	<div class="w-100">
+		Cpu Frequencies
+		<div id="lineChart" @mouseover="hover=true" @mouseleave="hover=false"></div>
+	</div>
 </template>
 
 <script>
-import Chart from 'chart.js';
+import uPlot from 'uplot';
 
 export default {
 	name: 'LineChart',
 	props: {
-		options: {
-			type: Object,
-			default: null
-    	},
+		options: Object,
 		chartdata: {
-			type: Object,
+			type: Array,
 			default: null
     	},
-		minvalue: {
-			type: Date,
-			default: null
-		}
   	},
 
 	data () {
 		return {
-			chart: null,
 			ctx: null,
-			hover: false
+			chart: null,
+			hover: false,
+			max: 0
 		}
 	},
 	
 	watch: {
 		chartdata: function (newData, oldData) {
-			if (oldData) {
-				this.chart.update();
-			} else {
-				if (this.chart) {
-					this.chart.destroy();
+			if (oldData == null || !this.chart) {
+				this.createChart(newData);
+			} else if (!this.hover && this.chart) {
+				// If the newData[1] contains more than 4000 items, use a for loop
+				// https://medium.com/coding-at-dawn/the-fastest-way-to-find-minimum-and-maximum-values-in-an-array-in-javascript-2511115f8621
+				let max = Math.max.apply(null, newData[1]);
+				if (this.max != max) {
+					this.chart.setScale('y', { min: 0, max: (max + (max / 10)) });
 				}
-				if (this.ctx) {
-					this.chart = new Chart(this.ctx, {
-						type: 'line',
-						data: this.chartdata,
-						options: this.options
-					})
-				}
+				this.chart.setData(newData);
 			}
-		},
-		minvalue: function (newData, oldData) {
-			this.chart.options.scales.xAxes[0].ticks.min = newData;
-			this.chart.update();
 		}
 	},
 
 	mounted () {
-		if (this.chart == null) {
-			this.ctx = document.getElementById('lineChart').getContext('2d');
-			this.chart = new Chart(this.ctx, {
-				type: 'line',
-				data: this.chartdata,
-				options: this.options
-			});
+		if (this.ctx == null) {
+			this.ctx = document.getElementById('lineChart');
 		}
+
+		this.$nextTick(function() {
+			window.addEventListener('resize', this.setChartSize);
+		});
+	},
+
+	methods: {
+		createChart: function(data) {
+			this.chart = new uPlot(this.options, data, this.ctx);
+		},
+		getSize: function() {
+			return {
+				width: window.innerWidth,
+				height: 300,
+			}
+		},
+		setChartSize: function (event) {
+			if (this.chart) {
+				this.chart.setSize(this.getSize());
+			}
+		}
+	},
+
+	beforeDestroy: function() {
+		window.removeEventListener('resize', this.setChartSize);
 	}
 }
 </script>
+
+<style scoped>
+@import './../assets/uPlot.min.css';
+</style>

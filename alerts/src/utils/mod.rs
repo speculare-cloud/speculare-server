90acecfb5f3a0dcfa6440d4248db671cca9f0b72
Martichou <martichou.andre@gmail.com>
2021-07-13 19:35:11 +0200

    Add some comments

--- alerts/src/utils/mod.rs
+++ alerts/src/utils/mod.rs
@@ -51,15 +51,21 @@ pub const DISALLOWED_STATEMENT: &[&str] = &[
     "ROLLBACK",
 ];
 
+/// Create the query for the Alert and get the QueryType
 pub fn construct_query(alert: &Alerts) -> (String, QueryType) {
+    // Split the lookup String from the alert for analysis
     let mut lookup_parts = alert.lookup.split(' ');
 
+    // The type of the query
+    // this is pretty much the aggregation function Postgres is going to use
     let req_type = if let Some(aggr) = lookup_parts.next() {
         aggr
     } else {
         panic!("Not aggregation function defined");
     };
 
+    // Determine the mode of the query
+    // The mode is for now, either Pct or Abs
     let req_mode = if let Some(mode) = lookup_parts.next() {
         match mode {
             "pct" => QueryType::Pct,
@@ -70,21 +76,29 @@ pub fn construct_query(alert: &Alerts) -> (String, QueryType) {
         panic!("No mode defined in the query")
     };
 
+    // Get the timing of the query, that is the interval range
+    // -> meaning we'll get req_time past datas and get the req_type.
     let req_time = if let Some(time) = lookup_parts.next() {
         time
     } else {
         panic!("Can't determine the time range");
     };
 
+    // This is the columns we ask for in the first place, this value is mandatory
     let req_one = lookup_parts.nth(1);
 
+    // Construct the SELECT part of the query
     let mut pg_select = String::new();
     let select_cols = req_one.unwrap().split(',');
     for col in select_cols {
+        // We're casting everything to float8 to handle pretty much any type we need
         pg_select.push_str(&format!("{}({})::float8 + ", req_type, col));
     }
+    // Remove the last " + "
     pg_select.drain(pg_select.len() - 3..pg_select.len());
+    // Based on the mode, we might need to do some different things
     match req_mode {
+        // For pct we need to define numerator and divisor.
         QueryType::Pct => {
             // req_two only exists if req_mode == Pct
             let req_two = lookup_parts.nth(1);
@@ -97,24 +111,32 @@ pub fn construct_query(alert: &Alerts) -> (String, QueryType) {
             pg_select.drain(pg_select.len() - 3..pg_select.len());
             pg_select.push_str(" as divisor");
         }
+        // For abs we just need to define the addition of all columns as value
         QueryType::Abs => {
             pg_select.push_str(" as value");
         }
     }
 
+    // Optional where clause
+    // Allow us to add a WHERE condition to the query if needed
     let mut pg_where = String::new();
     if alert.where_clause.is_some() {
         pg_where.push_str(" AND ");
         pg_where.push_str(alert.where_clause.as_ref().unwrap());
     };
 
+    // Base of the query, we plug every pieces together here
     let query = format!("SELECT time_bucket('{0}', created_at) as time, {1} FROM {2} WHERE host_uuid=$1 AND created_at > now() at time zone 'utc' - INTERVAL '{0}' {3} GROUP BY time ORDER BY time DESC", req_time, pg_select, alert.table, pg_where);
 
     trace!("Query[{:?}] is {}", req_mode, &query);
     (query, req_mode)
 }
 
+/// This function execute the query based on the QueryType,
+/// because all type does not wait for the same result.
 pub fn execute_query(query: &str, host_uuid: &str, qtype: &QueryType, conn: &ConnType) -> String {
+    // Based on the type we decide which way to go
+    // Each type has their own return structure and convertion method (from struct to String).
     match qtype {
         QueryType::Pct => {
             let results = sql_query(query)
@@ -137,10 +159,15 @@ pub fn execute_query(query: &str, host_uuid: &str, qtype: &QueryType, conn: &Con
     }
 }
 
+/// This function is the core of the monitoring, this is where we:
+/// - Execute the query and get the result
+/// - Evaluate if we need to trigger an incidents or not
 pub fn execute(query: &str, alert: &Alerts, qtype: &QueryType, conn: &ConnType) {
+    // Execute the query passed as arguement (this query was build previously)
     let result = execute_query(query, &alert.host_uuid, qtype, conn);
     trace!("{}", &result);
 
+    // Determine if we are in a Warn or Crit level of incidents
     let should_warn = eval_boolean(&alert.warn.replace("$this", &result));
     let should_crit = eval_boolean(&alert.crit.replace("$this", &result));
     trace!("{:?}, {:?}", should_warn, should_crit);

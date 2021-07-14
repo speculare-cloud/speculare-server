7c3c70cd9d88617db0b9fc77519bcf03e0c486cb
Martichou <martichou.andre@gmail.com>
2021-07-14 14:44:34 +0200

    Revert "to-revert: code for when Rustls will reach 0.20"
    This reverts commit 4154b9c4582578da87cb305172e6227ba8475501.

--- sproot/src/lib.rs
+++ sproot/src/lib.rs
@@ -8,7 +8,8 @@ pub mod errors;
 pub mod models;
 
 use diesel::{prelude::PgConnection, r2d2::ConnectionManager};
-use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls::internal::pemfile::{certs, pkcs8_private_keys, rsa_private_keys};
+use rustls::{NoClientAuth, ServerConfig};
 use std::fs::File;
 use std::io::BufReader;
 
@@ -32,36 +33,27 @@ pub fn configure_logger(level: String) {
 ///
 /// Use key and cert for the path to find the files.
 pub fn get_ssl_builder(key: String, cert: String) -> ServerConfig {
+    // Init the ServerConfig with no Client's cert verifiction
+    let mut config = ServerConfig::new(NoClientAuth::new());
     // Open BufReader on the key and cert files to read their content
     let cert_file = &mut BufReader::new(
-        File::open(&cert).expect(&format!("Certificate file not found at {}", cert)),
+        File::open(&cert).unwrap_or_else(|_| panic!("Certificate file not found at {}", cert)),
     );
-    let key_file = &mut BufReader::new(
-        File::open(&key).expect(&format!("Key file not found at {}", key))
-    );
+    let key_file =
+        &mut BufReader::new(File::open(&key).unwrap_or_else(|_| panic!("Key file not found at {}", key)));
     // Create a Vec of certificate by extracting all cert from cert_file
-    let cert_chain = rustls_pemfile::certs(cert_file)
-        .unwrap()
-        .iter()
-        .map(|v| Certificate(v.clone()))
-        .collect();
-    
+    let cert_chain = certs(cert_file).unwrap();
     // Extract all PKCS8-encoded private key from key_file and generate a Vec from them
-    let mut keys = rustls_pemfile::pkcs8_private_keys(key_file).unwrap();
+    let mut keys = pkcs8_private_keys(key_file).unwrap();
     // If no keys are found, we try using the rsa type
     if keys.is_empty() {
         // Reopen a new BufReader as pkcs8_private_keys took over the previous one
         let key_file =
-            &mut BufReader::new(File::open(&key).expect(&format!("Key file not found at {}", key)));
-        keys = rustls_pemfile::rsa_private_keys(key_file).unwrap();
+            &mut BufReader::new(File::open(&key).unwrap_or_else(|_| panic!("Key file not found at {}", key)));
+        keys = rsa_private_keys(key_file).unwrap();
     }
-    // Convert the first key to be a PrivateKey
-    let key: PrivateKey = PrivateKey(keys.remove(0));
+    // Set a single certificate to be used for all subsequent request
+    config.set_single_cert(cert_chain, keys.remove(0)).unwrap();
 
-    // Init the ServerConfig with no Client's cert verifiction
-    ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_single_cert(cert_chain, key)
-        .expect("bad certificate/key")
+    config
 }
